db68bc0ddd979fb2d4f5e55389d42c351974f598
guruputranamd <rajkumar.kesavaiyer@amd.com>
2022-04-19 23:31:33 +0800

    first one

---
+++ src/App.jsx
@@ -0,0 +1,64 @@
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import InputField from "./components/InputField";
+import TodoList from "./components/TodoList";
+import { DragDropContext } from "react-beautiful-dnd";
+const App = () => {
+    const [todo, setTodo] = useState("");
+    const [todos, setTodos] = useState([]);
+    const [CompletedTodos, setCompletedTodos] = useState([]);
+    const handleAdd = (e) => {
+        e.preventDefault();
+        if (todo) {
+            setTodos([...todos, { id: Date.now(), todo, isDone: false }]);
+            setTodo("");
+        }
+    };
+    const onDragEnd = (result) => {
+        const { destination, source } = result;
+        if (!destination) {
+            return;
+        }
+        if (destination.droppableId === source.droppableId &&
+            destination.index === source.index) {
+            return;
+        }
+        let add;
+        let active = todos;
+        let complete = CompletedTodos;
+        // Source Logic
+        if (source.droppableId === "TodosList") {
+            add = active[source.index];
+            active.splice(source.index, 1);
+        }
+        else {
+            add = complete[source.index];
+            complete.splice(source.index, 1);
+        }
+        // Destination Logic
+        if (destination.droppableId === "TodosList") {
+            active.splice(destination.index, 0, add);
+        }
+        else {
+            complete.splice(destination.index, 0, add);
+        }
+        setCompletedTodos(complete);
+        setTodos(active);
+        console.log("Todos", todos);
+        console.log("Completed Todos", CompletedTodos);
+    };
+    useEffect(() => {
+        // Update the document title using the browser API
+        console.log("Triggered !!");
+        console.log("Todos", todos);
+        console.log("Completed Todos", CompletedTodos);
+    }, [todos, CompletedTodos]);
+    return (<DragDropContext onDragEnd={onDragEnd}>
+      <div className="App">
+        <span className="heading">Visual Task Manager</span>
+        <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd}/>
+        <TodoList todos={todos} setTodos={setTodos} CompletedTodos={CompletedTodos} setCompletedTodos={setCompletedTodos}/>
+      </div>
+    </DragDropContext>);
+};
+export default App;
